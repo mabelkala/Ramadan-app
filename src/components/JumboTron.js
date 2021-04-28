3d35e5beef3f4f3be645fac28cf224dff3537d4e
mabelkala <mabelkala@gmail.com>
2021-04-28 09:03:43 +0000

    changed some files

--- src/components/JumboTron.js
+++ src/components/JumboTron.js
@@ -6,7 +6,7 @@ function JumboTron(){
         Ramadan Kareem! May Allah accept your prayers and rozas this Ramzan! Wish you a very Happy Ramzan 2021! May Allah protect you from all sins and bring you peace, joy, and hope.
         Ramadan Kareem! May Allah accept your prayers and rozas this Ramzan! Wish you a very Happy Ramzan 2021! May Allah protect you from all sins and bring you peace, joy, and hope.
         </h3>
-        <button>LEARN MORE ABOUT ISLAM</button>
+        <button className="btn-hero">LEARN MORE ABOUT ISLAM</button>
     </header>
     )
 }
